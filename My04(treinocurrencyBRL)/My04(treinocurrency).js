36fc5036241b486051e5dd502b780553b20ff775
Ariel Fagundes <arieldss6@gmail.com>
2024-07-21 12:12:35 -0300

    Arquivos treino Currency

---
+++ My04(treinocurrencyBRL)/My04(treinocurrency).js
@@ -0,0 +1,6 @@
+function envioform() {
+    const nome = document.getElementById('nome').value
+    const sal = Number(document.getElementById('valor').value)
+    const real = sal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+    const confirm = window.confirm(`${nome}, Sua pretensão salarial é: ${real}.`)
+}
