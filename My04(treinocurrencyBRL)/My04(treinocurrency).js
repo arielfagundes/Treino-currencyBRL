a951069912f41e42072aac013924bbb7308941cb
Ariel Fagundes <arieldss6@gmail.com>
2024-07-21 12:38:32 -0300

    Update My04(treinocurrency).js

--- My04(treinocurrencyBRL)/My04(treinocurrency).js
+++ My04(treinocurrencyBRL)/My04(treinocurrency).js
@@ -1,6 +1,7 @@
-function envioform() {
+function envioform() { //Executa a função
     const nome = document.getElementById('nome').value
     const sal = Number(document.getElementById('valor').value)
     const real = sal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
     const confirm = window.confirm(`${nome}, Sua pretensão salarial é: ${real}.`)
 }
+
